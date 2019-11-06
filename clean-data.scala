828a8a8984a7ca76d012fe1074aef66af667caa6
lingxuan925 <tc15gn@brocku.ca>
2019-11-06 00:16:01 -0500

    clean data now works, but some problems with join, need to rethink which tables to joins

--- clean-data.scala
+++ clean-data.scala
@@ -7,6 +7,9 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import scala.util.matching._
+import org.apache.spark.sql.types._
+import java.sql.Timestamp
+import org.apache.hadoop.fs._
 
 object CleanData {
   def main(args: Array[String]) {
@@ -17,44 +20,63 @@ object CleanData {
 
     val inputPath = args(0)
     val outputPath = args(1)
-
+    val outputDir = new Path(outputPath)
     val sc = new SparkContext(new SparkConf().setAppName("Clean Data"))
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-    val df_questions = sqlContext.read.format("csv").option("header", "true").load(inputPath+"*questions*")
-    val df_posts = sqlContext.read.format("csv").option("header", "true").load(inputPath+"stackoverflow*")
-    val df_users = sqlContext.read.format("csv").option("header", "true").load(inputPath+"users*")
-    val df_votes = sqlContext.read.format("csv").option("header", "true").load(inputPath+"votes*")
+    val conf = sc.hadoopConfiguration
+    conf.set("google.cloud.auth.service.account.enable", "true")
+    conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
+    conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
+    val gcsFS = new Path("gs://stackoverflow_bigdata/").getFileSystem(conf)
+
+    gcsFS.delete(outputDir, true)
+
+    val df_questions = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(inputPath+"*questions*")
+    val df_posts = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").option("nullValue", "null").load(inputPath+"stackoverflow*")
+    val df_users = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(inputPath+"users*")
+    val df_votes = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(inputPath+"votes*")
 
     val df_questions_distinct = df_questions.distinct()
     val df_posts_distinct = df_posts.distinct()
     val df_users_distinct = df_users.distinct()
     val df_votes_distinct = df_votes.distinct()
-/*
+
+    //df_posts_distinct.show()
+    //df_users_distinct.show() 
+
     val df_posts_join_users = df_posts_distinct.as("posts").join(df_users_distinct.as("users"), col("posts.owner_user_id") === col("users.id"), "inner")
       .select(col("posts.id"), col("posts.title"), col("posts.creation_date"), col("posts.answer_count"),col("posts.favorite_count"), col("posts.score"), col("posts.tags"), col("posts.view_count"), col("posts.owner_user_id"), col("users.reputation"))
 
 //    val df_posts_join_users_votes = df_posts_join_users.as("postsUsers").join(df_votes.as("votes"), col("postsUsers.id") === col("votes.post_id"), "inner").drop("id", "creation_date", "post_id")
     val df_posts_join_users_votes = df_posts_join_users.as("postsUsers").join(df_votes_distinct.as("votes"), col("postsUsers.id") === col("votes.post_id"), "inner")
       .select(col("postsUsers.id"), col("postsUsers.title"), col("postsUsers.creation_date"), col("postsUsers.answer_count"),col("postsUsers.favorite_count"), col("postsUsers.score"), col("postsUsers.tags"), col("postsUsers.view_count"), col("postsUsers.owner_user_id"), col("postsUsers.reputation"), col("votes.vote_type_id"))
-      .orderBy(desc("postsUsers.answer_count"))
+      .orderBy("postsUsers.answer_count")
 
     df_posts_join_users_votes.show()
-*/
+
     val df_questions_join_users = df_questions_distinct.as("questions").join(df_users_distinct.as("users"), col("questions.owner_user_id") === col("users.id"), "inner")
       .select(col("questions.id"), col("questions.title"), col("questions.creation_date"), col("questions.answer_count"),col("questions.favorite_count"), col("questions.score"), col("questions.tags"), col("questions.view_count"), col("questions.owner_user_id"), col("users.reputation"))
+//    df_questions_join_users.show()
 
     val df_questions_join_users_votes = df_questions_join_users.as("questionsUsers").join(df_votes_distinct.as("votes"), col("questionsUsers.id") === col("votes.post_id"), "inner")
       .select(col("questionsUsers.id"), col("questionsUsers.title"), col("questionsUsers.creation_date"), col("questionsUsers.answer_count"),col("questionsUsers.favorite_count"), col("questionsUsers.score"), col("questionsUsers.tags"), col("questionsUsers.view_count"), col("questionsUsers.owner_user_id"), col("questionsUsers.reputation"), col("votes.vote_type_id"))
-//      .orderBy(desc("questionsUsers.answer_count"))
+      .orderBy("questionsUsers.answer_count")
 
+    df_questions_join_users_votes.show()
+/*
     val expr = "[0-9]".r
     val df_questions_join_users_votes_clean = df_questions_join_users_votes
-//      .filter(!col("answer_count").rlike(expr))
-      .filter(row => row.getAs[String]("answer_count").matches("""\d+"""))
+      .filter(row => row match {
+        case Row(id: Integer, title: String, creation_date: Timestamp, answer_count: Integer, favourite_count: Integer, score: Integer, tags: String, view_count: Integer, owner_user_id: Integer, reputation:  Integer, vote_type_id: Integer) => true
+        case _ => false
+      })
+*/
+      //.filter(row => row.getAs[String]("answer_count").matches("""\d+"""))
 
-    df_questions_join_users_votes_clean.show()
+//    df_questions_join_users_votes_clean.show()
 //    df_questions_join_users_votes.select("*").where(df_questions_join_users_votes.col("id") === 56274647).show()
-
+    df_questions_join_users_votes.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"question_new.csv")
+    df_posts_join_users_votes.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"posts_new.csv")
   }
 }
