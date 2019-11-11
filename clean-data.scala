7d81bf63976031883f247e9eb56f91aa0936934a
lingxuan925 <tc15gn@brocku.ca>
2019-11-10 21:39:10 -0500

    fixing dirty rows after including body column in progress

--- clean-data.scala
+++ clean-data.scala
@@ -42,9 +42,24 @@ object CleanData {
     val df_users_distinct = df_users.distinct()
     val df_votes_distinct = df_votes.distinct()
 
-    //df_posts_distinct.show()
-    //df_users_distinct.show() 
+    val df_questions_correctType = df_questions_distinct.selectExpr("cast(id as int) id", 
+                        "cast(title as String) title", 
+                        "cast(body as String) body", 
+                        "cast(creation_date as String) creation_date", 
+                        "cast(answer_count as int) answer_count",
+                        "cast(favorite_count as int) favorite_count",
+                        "cast(score as int) score",
+                        "cast(tags as int) tags",
+                        "cast(view_count as int) view_count",
+                        "cast(owner_user_id as int) owner_user_id")
+    //df_questions_correctType.printSchema()
+    //df_questions_correctType.show()
 
+    val df_users_correctType = df_users_distinct.selectExpr("cast(id as int) id",
+                        "cast(reputation as int) reputation")
+    val df_votes_correctType = df_votes_distinct.selectExpr("cast(post_id as int) post_id",
+                        "cast(vote_type_id as int) vote_type_id")
+/*
     val df_posts_join_users = df_posts_distinct.as("posts").join(df_users_distinct.as("users"), col("posts.owner_user_id") === col("users.id"), "inner")
       .select(col("posts.id"), col("posts.body"), col("posts.title"), col("posts.creation_date"), col("posts.answer_count"),col("posts.favorite_count"), col("posts.score"), col("posts.tags"), col("posts.view_count"), col("posts.owner_user_id"), col("users.reputation"))
 
@@ -54,31 +69,31 @@ object CleanData {
       .orderBy("postsUsers.answer_count")
       .distinct()
       .withColumn("label", lit(1))
-
+*/
 //    df_posts_join_users_votes.show()
-
-    val df_questions_join_users = df_questions_distinct.as("questions").join(df_users_distinct.as("users"), col("questions.owner_user_id") === col("users.id"), "inner")
+    val df_questions_join_users = df_questions_correctType.as("questions").join(df_users_correctType.as("users"), col("questions.owner_user_id") === col("users.id"), "inner")
       .select(col("questions.id"), col("questions.title"), col("questions.body"), col("questions.creation_date"), col("questions.answer_count"),col("questions.favorite_count"), col("questions.score"), col("questions.tags"), col("questions.view_count"), col("questions.owner_user_id"), col("users.reputation"))
 //    df_questions_join_users.show()
 
-    val df_questions_join_users_votes = df_questions_join_users.as("questionsUsers").join(df_votes_distinct.as("votes"), col("questionsUsers.id") === col("votes.post_id"), "inner")
+    val df_questions_join_users_votes = df_questions_join_users.as("questionsUsers").join(df_votes_correctType.as("votes"), col("questionsUsers.id") === col("votes.post_id"), "inner")
       .select(col("questionsUsers.id"), col("questionsUsers.title"), col("questionsUsers.body"), col("questionsUsers.creation_date"), col("questionsUsers.answer_count"),col("questionsUsers.favorite_count"), col("questionsUsers.score"), col("questionsUsers.tags"), col("questionsUsers.view_count"), col("questionsUsers.owner_user_id"), col("questionsUsers.reputation"), col("votes.vote_type_id"))
       .orderBy("questionsUsers.answer_count")
       .distinct()
-      .withColumn("label", lit(0))
+      .withColumn("label", lit(0).cast(IntegerType))
 
-//    df_questions_join_users_votes.show()
     val df_questions_join_users_votes_clean = df_questions_join_users_votes
-      .filter(row => row match {
-        case Row(id: Integer, title: String, creation_date: Timestamp, answer_count: Integer, favourite_count: Integer, score: Integer, tags: String, view_count: Integer, owner_user_id: Integer, reputation:  Integer, vote_type_id: Integer) => true
-        case _ => false
+      /*.filter(row => row match {
+        case Row(id: Integer, title: String, body: String, creation_date: String, answer_count: Integer, favourite_count: Integer, score: Integer, tags: String, view_count: Integer, owner_user_id: Integer, reputation:  Integer, vote_type_id: Integer, label: Integer) => false 
+        case _ => true
       })
-      //.filter(row => row.getAs[String]("answer_count").matches("""\d+"""))
+*/
+      .filter(row => {
+        row.getAs("answer_count").isInstanceOf[String]
+      })
 
 //    df_questions_join_users_votes.select("*").where(df_questions_join_users_votes.col("id") === 56274647).show()
-//    df_questions_join_users_votes.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"question_new.csv")
-//    df_posts_join_users_votes.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"posts_new.csv")
-    df_questions_join_users_votes.repartition(8).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"question_new.csv")
-    df_posts_join_users_votes.repartition(8).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"posts_new.csv")
+
+    df_questions_join_users_votes_clean.repartition(8).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"question_new.csv")
+//    df_posts_join_users_votes.repartition(8).write.format("com.databricks.spark.csv").option("header", "true").save(outputPath+"posts_new.csv")
   }
 }
